5243e9410ebe36e01003dcd61d3fff7b084dce9a
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2020-02-22 23:11:52 +0100

    Add User Details

---
+++ data/js/user_details.js
@@ -0,0 +1,8 @@
+var user_details = {
+    "screen_name": "prgrnr",
+    "location": "",
+    "full_name": "Wanderlust",
+    "bio": "Mit Campervan, Drahtesel und auf Schusters Rappen durch die Welt.\n// Tweets by @_meyse_ // © CC BY 4.0",
+    "id": "2807406332",
+    "created_at": "2014-09-13 12:34:50 +0000"
+}
